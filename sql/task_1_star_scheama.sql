56131a7e48722dbc3bc2513653c46ea2532f2ad3
Dein Name <deine@email.de>
2026-01-11 21:17:48 +0100

    revided Star Schema

--- sql/task_1_star_scheama.sql
+++ sql/task_1_star_scheama.sql
@@ -19,11 +19,7 @@ CREATE TABLE dim_time (
     time_id     BIGSERIAL PRIMARY KEY,
     db_time_str TEXT NOT NULL UNIQUE,
     ts          TIMESTAMPTZ NOT NULL,
-    year        INT NOT NULL,
-    month       INT NOT NULL,
-    day         INT NOT NULL,
     hour        INT NOT NULL,
-    minute      INT NOT NULL,
     dow         INT NOT NULL
 );
 
@@ -40,11 +36,8 @@ CREATE TABLE dim_snapshot (
 --Fourth Dim Train
 CREATE TABLE dim_train ( 
     train_id        BIGSERIAL PRIMARY KEY,
-    operator_code   TEXT,
     category        TEXT,
     train_number    TEXT,
-    train_type      TEXT,
-    train_flag      TEXT,
     UNIQUE NULLS NOT DISTINCT (operator_code, category, train_number, train_type, train_flag)
 );
 
@@ -55,20 +48,13 @@ CREATE TABLE fact_train_movement (
 
     station_id      BIGINT NOT NULL REFERENCES dim_station(station_id),
     train_id        BIGINT REFERENCES dim_train(train_id),
-
     snapshot_id    BIGINT NOT NULL REFERENCES dim_snapshot(snapshot_id),
-
     stop_natural_id TEXT NOT NULL,
 
     planned_arrival_time_id     BIGINT REFERENCES dim_time(time_id),
     planned_departure_time_id   BIGINT REFERENCES dim_time(time_id),
-    
     current_arrival_time_id     BIGINT REFERENCES dim_time(time_id),
     current_departure_time_id   BIGINT REFERENCES dim_time(time_id),
-
-    platform_planned TEXT,
-    platform_current TEXT,
-    line_code        TEXT,
 
     arrival_delay_min    INT,
     departure_delay_min  INT,
